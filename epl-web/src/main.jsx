b83aa54b74abd3b06fbae16af48fb14dc303072d
duongvu4 <vucongtuanduong4@gmail.com>
2025-02-17 09:45:06 +0000

    update

--- epl-web/src/main.jsx
+++ epl-web/src/main.jsx
@@ -1,13 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+// import './style/global.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  }
+]);
 
-// hello-test
-// hello test2
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
---
+++ epl-web/src/pages/register.jsx
@@ -0,0 +1,8 @@
+const RegisterPage = () => {
+    return (
+        <>
+            Register Page
+        </>
+    )
+}
+export default RegisterPage;
