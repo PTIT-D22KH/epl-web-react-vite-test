63e0c8db8e983675a644b2f5c66cf27939dcc70e
duongvu4 <vucongtuanduong4@gmail.com>
2025-02-17 07:50:18 +0000

    update

---
+++ epl-web/src/components/layout/header.jsx
@@ -0,0 +1,86 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Menu, message } from "antd";
+import {
+  UsergroupAddOutlined,
+  LoginOutlined,
+  AliwangwangOutlined,
+  HomeOutlined,
+  AuditOutlined,
+} from "@ant-design/icons";
+import { useContext, useState } from "react";
+import { AuthContext } from "../context/auth.context";
+import { logoutAPI } from "../../services/api.service";
+const Header = () => {
+  const [current, setCurrent] = useState("");
+  const navigate = useNavigate();
+
+  const {user, setUser} = useContext(AuthContext);
+
+
+  const onClick = (e) => {
+    setCurrent(e.key);
+  };
+
+  const handleLogout = async() =>{
+    const res =  await logoutAPI();
+    if(res.data){
+      // clear data
+      localStorage.removeItem("access_token");
+      setUser({
+        "email": "",
+        "fullName": "",
+        "role": "",
+        "id": ""
+      })
+      message.success("Logout thành công.")
+
+    // redirect to home
+      navigate("/");
+    }
+
+  }
+  const items = [
+    {
+      label: <Link to={"/"}>Home</Link>,
+      key: "home",
+      icon: <HomeOutlined />,
+    },
+    {
+      label: <Link to={"/users"}>Users</Link>,
+      key: "users",
+      icon: <UsergroupAddOutlined />,
+    },
+    {
+      label: <Link to={"/books"}>Books</Link>,
+      key: "products",
+      icon: <AuditOutlined />,
+    },
+    ...(!user.id ? [{
+      label: <Link to={"/login"}>Đăng nhập</Link>,
+      key: 'login',
+      icon:<LoginOutlined />,
+    }] : []),
+
+    ...(user.id  ? [{
+      label: `Welcome ${user.fullName}`,
+      key: "setting",
+      icon:<AliwangwangOutlined />,
+     children: [
+      {
+        label: <span onClick={() => handleLogout()}>Đăng xuất</span>,
+        key: 'logout',
+      },
+     ]
+    },] : []),
+    
+  ];
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
+export default Header;
